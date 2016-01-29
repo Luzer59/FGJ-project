0897226a7fd0230bb5040872a6f8fdf8f8254df5
LeeviPaa <LeeviPaa@AOTA1-1L155-11D.kamit.fi>
2016-01-29 23:09:20 +0200

    Lisätty enemy prefabeja
    Enemy spawner ja script. Enemy ja enemy behaviour script.

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+
+	// Use this for initialization
+    public Transform SpawnerTransform;
+    public GameObject Enemy;
+    public int spawnInterval = 10;
+    public int startTime = 10;
+	void Start () 
+    {
+        StartCoroutine(spawnEnemies(spawnInterval, startTime));
+	}
+	IEnumerator spawnEnemies(int spawnInterval, int startTime)
+    {
+        yield return new WaitForSeconds(startTime);
+        Instantiate(Enemy, SpawnerTransform.position, SpawnerTransform.rotation);
+        yield return new WaitForSeconds(spawnInterval);
+    }
+
+}
