4ac5b5931fb8c127d80398a522eb39bfbdb72aae
Luzer59 <luzer59@hotmail.com>
2016-01-29 22:31:14 +0200

    Testimaasto, pelaaja ja yksi tuli skillin tapainen juttu

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Transform moveTarget;
+    public float moveTargetCompleteDistance;
+    public float moveSpeed;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                moveTarget.position = hit.point;
+            }
+        }
+
+        moveTarget.position = new Vector3(moveTarget.position.x, transform.position.y, moveTarget.position.z);
+
+        if (Vector3.Distance(transform.position, moveTarget.position) > moveTargetCompleteDistance)
+        {
+            transform.LookAt(moveTarget);
+            transform.Translate(transform.forward * moveSpeed, Space.World);
+        }
+    }
+}
