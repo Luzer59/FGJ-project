60f7b60c50030e9b78766cf39ed3a8b2ddd38933
LeeviPaa <LeeviPaa@AOTA1-1L155-11D.kamit.fi>
2016-01-31 00:08:06 +0200

    Lisätty campfire ja camera transformit

---
+++ Assets/CAM.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class CAM : MonoBehaviour
+{
+
+    void OnGUI()
+    {
+        EditorGUILayout.TextField("SceneViewCameraAngle", "" + SceneView.lastActiveSceneView.rotation);
+    }
+}
---
+++ Assets/LightFlicker.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightFlicker : MonoBehaviour {
+
+    public float duration = 1.0F;
+    public Light lt;
+    void Start()
+    {
+        lt = GetComponent<Light>();
+    }
+    void Update()
+    {
+        float phi = Time.time / duration * 2 * Mathf.PI;
+        float amplitude = Mathf.Cos(phi) * 0.75F + 6.0F;
+        lt.intensity = amplitude;
+    }
+}
