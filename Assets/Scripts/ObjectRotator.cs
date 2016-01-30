cc3e82c789f10e763dcccabbc85720dc4e7cb816
Endoperez <janne.joensuu@gmail.com>
2016-01-30 02:10:55 +0200

    VFX and stuff
    Mukana iso kasa .meta -tiedostoja. Hyvä vai huono?
    
    Uusia materiaaleja: terrain, tuli, jää.
    Uusia prefabeja: jääpuikot, tuliefekti, jää-projektori,
    vesilammikko-projektori.
    Uusia malleja - Pumpkin, Icicles,

---
+++ Assets/Scripts/ObjectRotator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectRotator : MonoBehaviour {
+
+    public Vector3 rotationSpeed = new Vector3( 1.0f, 0.0f, 0.0f );
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(rotationSpeed * Time.deltaTime, Space.Self);
+	}
+
+}
