fc7fd6122c23e0a670f19b5b4622b476b6eb9069
Endoperez <janne.joensuu@gmail.com>
2016-01-31 12:36:00 +0200

    Mouse Pointer Projector for tutorial
    add Mouse Pointer Projector to the game scene.

---
+++ Assets/Scripts/MouseTutorialController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseTutorialController : MonoBehaviour {
+
+	public Projector projector;
+	public GameObject particlesToShowOnDisable;
+
+	public float animationSpeed = 1.0f;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		projector.orthographicSize += animationSpeed * Time.deltaTime;
+		
+		if(projector.orthographicSize > 10f || projector.orthographicSize < 6f) {
+			animationSpeed *= -1f;
+		}
+	
+	
+	}
+	
+	void OnTriggerEnter( Collider other) 
+	{
+		if(other.tag == "Player") {
+			DisableMe();
+		}
+	
+	}
+	
+	void DisableMe() 
+	{
+		GameObject.Instantiate(particlesToShowOnDisable, transform.position + new Vector3(0f,-6f,0f), transform.rotation);
+		gameObject.SetActive(false);
+	}
+	
+}
