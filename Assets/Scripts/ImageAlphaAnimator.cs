e90f537cd63949de0a7a8f45d9aca93d5d4bf727
Endoperez <janne.joensuu@gmail.com>
2016-01-31 13:53:06 +0200

    New Prefab for UI spells

---
+++ Assets/Scripts/ImageAlphaAnimator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ImageAlphaAnimator : MonoBehaviour {
+	public float animationSpeed = 0.11f;
+	private Image imgComponent;
+	
+	// Use this for initialization
+	void Start () {
+		imgComponent = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Color old = imgComponent.color;
+		imgComponent.color = new Color( old.r, old.g, old.b, old.a + animationSpeed * Time.deltaTime );
+		
+		if( imgComponent.color.a > 0.9f || imgComponent.color.a < 0.5f) {
+			animationSpeed *= -1f;
+		}
+	}
+}
